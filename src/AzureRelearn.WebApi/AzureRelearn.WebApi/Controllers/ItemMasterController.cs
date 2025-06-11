a983fe26af15568bbc0d022203e0e57450ad989b
amitjoshi <amitjoshipune@yahoo.com>
2025-06-11 10:32:07 +0530

    minor Changes in LoggingMiddleware for more line ****.  Changes for new ItemMasterController . WeatherForecast controller changed for  [HttpGet("GetForecast", Name="GetWeatherForecast")] - so that we can now call  public IEnumerable<WeatherForecast> Get() { ... } from  like  api/WeatherForecast/GetForecast

---
+++ src/AzureRelearn.WebApi/AzureRelearn.WebApi/Controllers/ItemMasterController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AzureRelearn.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemMasterController : ControllerBase
+    {
+        // GET: api/<ItemMasterController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ItemMasterController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ItemMasterController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ItemMasterController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ItemMasterController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- src/AzureRelearn.WebApi/AzureRelearn.WebApi/Middlewares/LoggingMiddleware.cs
+++ src/AzureRelearn.WebApi/AzureRelearn.WebApi/Middlewares/LoggingMiddleware.cs
@@ -10,9 +10,13 @@
 
         public async Task  Invoke(HttpContext context)
         {
+            Console.WriteLine("******************* Before next --> ***************************");
             Console.WriteLine($"LoggingMiddleware {context.Request.Method} {context.Request.Path}");
+            Console.WriteLine("******************* Before next <-- ***************************");
             await _next(context);
+            Console.WriteLine("******************** After next --> ***************************");
             Console.WriteLine($"LoggingMiddleware {context.Response.StatusCode}");
+            Console.WriteLine("******************** After next <-- ***************************");
         }
     }
 }
