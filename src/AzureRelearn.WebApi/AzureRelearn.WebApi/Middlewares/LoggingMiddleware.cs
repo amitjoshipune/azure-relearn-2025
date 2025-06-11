599b3cec490d0ccf62395a0535e2c8f9814f609a
amitjoshi <amitjoshipune@yahoo.com>
2025-06-11 14:02:16 +0530

    Adding ILogger to LoggingMiddleware

--- src/AzureRelearn.WebApi/AzureRelearn.WebApi/Middlewares/LoggingMiddleware.cs
+++ src/AzureRelearn.WebApi/AzureRelearn.WebApi/Middlewares/LoggingMiddleware.cs
@@ -3,20 +3,28 @@
     public class LoggingMiddleware
     {
         private readonly RequestDelegate _next;
-        public LoggingMiddleware(RequestDelegate next)
+        private ILogger<LoggingMiddleware> _logger;
+        public LoggingMiddleware(RequestDelegate next , ILogger<LoggingMiddleware> logger)
         {
             _next = next;
+           _logger = logger;
         }
 
         public async Task  Invoke(HttpContext context)
         {
             Console.WriteLine("******************* Before next --> ***************************");
+            _logger.LogInformation("******************* Before next --> ***************************");
             Console.WriteLine($"LoggingMiddleware {context.Request.Method} {context.Request.Path}");
+            _logger.LogInformation($"LoggingMiddleware {context.Request.Method} {context.Request.Path}");
             Console.WriteLine("******************* Before next <-- ***************************");
+            _logger.LogInformation("******************* Before next <-- ***************************");
             await _next(context);
             Console.WriteLine("******************** After next --> ***************************");
+            _logger.LogInformation("******************** After next --> ***************************");
             Console.WriteLine($"LoggingMiddleware {context.Response.StatusCode}");
+            _logger.LogInformation($"LoggingMiddleware {context.Response.StatusCode}");
             Console.WriteLine("******************** After next <-- ***************************");
+            _logger.LogInformation("******************** After next <-- ***************************");
         }
     }
 }
