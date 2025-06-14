3ca6de67edcfd493cebfb4fb8dc475737a6785b7
amitjoshi <amitjoshipune@yahoo.com>
2025-06-14 15:21:01 +0530

    minor text changes for '        public IEnumerable<WeatherForecast> Get() "

--- src/AzureRelearn.WebApi/AzureRelearn.WebApi/Controllers/WeatherForecastController.cs
+++ src/AzureRelearn.WebApi/AzureRelearn.WebApi/Controllers/WeatherForecastController.cs
@@ -26,7 +26,7 @@ namespace AzureRelearn.WebApi.Controllers
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = "Hello from local app " + Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = "Hello from local computer and not docker - " + Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
         }
