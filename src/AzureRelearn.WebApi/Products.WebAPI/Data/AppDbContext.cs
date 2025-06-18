c025cbcb9f1d2569bf366095d6f5437f436d76d0
amitjoshi <amitjoshipune@yahoo.com>
2025-06-18 15:00:50 +0530

    Started working on Product Web API - Microsoft.EntityFrameworkCore related changes and DTOs , Models , migration etc done

---
+++ src/AzureRelearn.WebApi/Products.WebAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.WebAPI.Models;
+
+namespace Products.WebAPI.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- src/AzureRelearn.WebApi/Products.WebAPI/Program.cs
+++ src/AzureRelearn.WebApi/Products.WebAPI/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Products.WebAPI.Data;
+
 namespace Products.WebAPI
 {
     public class Program
@@ -8,7 +11,10 @@ namespace Products.WebAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+            });
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
