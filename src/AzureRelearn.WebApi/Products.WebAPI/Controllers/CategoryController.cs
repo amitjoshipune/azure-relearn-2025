0a16c09b23d6bf1d8321396c7d65336b2ef4748d
amitjoshi <amitjoshipune@yahoo.com>
2025-06-19 20:33:08 +0530

    Completed Category Controller with GET, POST, PUT, DELETE methods

--- src/AzureRelearn.WebApi/Products.WebAPI/Controllers/CategoryController.cs
+++ src/AzureRelearn.WebApi/Products.WebAPI/Controllers/CategoryController.cs
@@ -39,5 +39,36 @@ namespace Products.WebAPI.Controllers
             return await _ctx.Categories.FindAsync(id) is Category c 
                 ? Ok(new CategoryDto { Id = c.Id, Name = c.Name, Description = c.Description }) : NotFound();
         }
+
+        [HttpPost("Add" ,Name ="AddCategory")]
+        public async Task<IActionResult> Post(CategoryDto categoryDto)
+        {
+
+            var c = new Category() { Name = categoryDto.Name, Description= categoryDto.Description };
+            _ctx.Categories.Add(c);
+            await _ctx.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get),new { id = c.Id },categoryDto);
+        }
+
+        [HttpPut("Update", Name ="UpdateCategory")]
+        public async Task<IActionResult> Put(CategoryDto categoryDto)
+        {
+            var c = await _ctx.Categories.FindAsync(categoryDto.Id);
+            if(c is null) return NotFound();    
+            c.Name = categoryDto.Name;  
+            c.Description = categoryDto.Description;
+            await _ctx.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("Delete", Name ="DeleteCategory")]
+        public async Task<IActionResult> Delete(CategoryDto categoryDto)
+        {
+            var c = await _ctx.Categories.FindAsync(categoryDto.Id);
+            if (c is null) return NotFound();
+            _ctx.Categories.Remove(c);
+            await _ctx.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
