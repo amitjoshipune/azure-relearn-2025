fceddbad8763ab70345d8051bab4d667de87d3f2
amitjoshi <amitjoshipune@yahoo.com>
2025-06-25 12:09:35 +0530

    Products.WebAPi- program.cs modified for supporting cors for integration with Angular application

--- src/AzureRelearn.WebApi/Products.WebAPI/Program.cs
+++ src/AzureRelearn.WebApi/Products.WebAPI/Program.cs
@@ -15,27 +15,66 @@ namespace Products.WebAPI
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
             });
+
+            // Enable CORS -
+            // Register CORS Policy in builder.Services
+            // define policy -- Place it right before builder.Services.AddControllers().
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
+
+            // Add Controllers for API functionality
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // add Custom Middleware over here
+
+            //// Configure the HTTP request pipeline.
+            //if (app.Environment.IsDevelopment() || app.Environment.IsProduction() || app.Environment.IsStaging())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+
+            // Map Controllers to ensure API endpoints work
+            app.UseRouting();
+            // Use CORS Middleware in Pipeline
+            // Right after app.UseRouting();
+            app.UseCors("AllowAll");
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            // Serve Swagger UI
             if (app.Environment.IsDevelopment() || app.Environment.IsProduction() || app.Environment.IsStaging())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
+            // Default route for simple testing
+            app.MapGet("/", () => "Hello from .NET 8 API with Health Checks + App Insights!");
 
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
+            // Run the application
             app.Run();
         }
     }
